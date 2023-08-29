ab102c7ef0fc474be57bab95f08ecd4b965e8ad9
Marcos Giraudi <crinomarcos@gmail.com>
2023-08-29 18:17:57 -0300

    commit

---
+++ TpAulico/src/tpaulico/Empresa.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpaulico;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Marcos
+ */
+public class Empresa {
+    
+    private String razonSocial;
+    private Integer cuit;
+    private ArrayList<Empleado> Empleados;
+
+    public Empresa(String razonSocial, int cuit) {
+        this.razonSocial = razonSocial;
+        this.cuit = cuit;
+    }
+
+    public Empresa() {
+    }
+
+    
+    public Empresa(String razonSocial, int cuit, ArrayList<Empleado> Empleados) {
+        this.razonSocial = razonSocial;
+        this.cuit = cuit;
+        this.Empleados = Empleados;
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public int getCuit() {
+        return cuit;
+    }
+
+    public void setCuit(int cuit) {
+        this.cuit = cuit;
+    }
+
+    public ArrayList<Empleado> getEmpleados() {
+        return Empleados;
+    }
+
+    public void setEmpleados(ArrayList<Empleado> Empleados) {
+        this.Empleados = Empleados;
+    }
+    
+    public void agregarEmpleado(Empleado e){
+        Empleados.add(e);
+    }
+    public void mostrarEmpleado(){
+        
+    }
+}
